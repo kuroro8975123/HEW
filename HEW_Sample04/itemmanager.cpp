f65add46e1bfe07b1bf22c6ae4ffc1142dc17cc1
otomo0105 <gk1tomoki@gmail.com>
2019-12-18 14:48:12 +0900

    あ

---
+++ HEW_Sample04/itemmanager.cpp
@@ -0,0 +1,23 @@
+/*======================
+	アイテム管理.cpp
+======================*/
+
+#include "itemmanager.h"
+
+//コンストラクタ
+ItemManager::ItemManager() {
+
+}
+//デストラクタ
+ItemManager::~ItemManager()
+{
+
+}
+//アイテムポップ関数
+void ItemManager::Popitem() {
+
+}
+//アイテムフラグのゲッター
+bool ItemManager::GetPopFrag() {
+	return Popitem;
+}
