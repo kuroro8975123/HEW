a6dd1cc07a33ba77ae25b72038a4b5a2a7567a97
kuroiwa takuya <kuroro4659@icloud.com>
2019-12-18 15:35:32 +0900

    救済

---
+++ HEW_Sample04/itemmanager.h
@@ -0,0 +1,24 @@
+/*======================
+	アイテム管理クラス
+======================*/
+
+#pragma once
+
+#include "item.h"
+
+class ItemManager
+{
+private:
+	ItemManager *pImanager;
+	bool		ispop;
+
+
+public:
+
+	ItemManager();
+	~ItemManager();
+
+	void Popitem();
+
+	//bool GetPopFrag();
+};
