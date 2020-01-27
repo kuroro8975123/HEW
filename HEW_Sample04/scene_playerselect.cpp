b841a50ceb790a1e3ef5542609d9b2697fbd29c6
NEPIA0605 <ryty1021@gmail.com>
2020-01-27 17:05:48 +0900

    更新

--- HEW_Sample04/scene_playerselect.cpp
+++ HEW_Sample04/scene_playerselect.cpp
@@ -4,23 +4,21 @@
 #include "sprite.h"
 #include "texture.h"
 #include "fade.h"
-<<<<<<< HEAD
-=======
 #include "enemy.h"
->>>>>>> e3085da7d0b9de7fddd580972e7adcc756bd4cd0
 #include "scene_playerselect.h"
 
 int Select_1P;
 int Select_2P;
 int Select_Count;
-float move_x;
-float move_y;
-float move;
+int frame;
 bool Select;
 bool Left;
 bool Right;
 
-D3DXVECTOR2 SelectPos;
+Select_Kaziki kaziki;
+Select_Kujira kuzira;
+Select_Iruka iruka;
+Select_Uma uma;
 
 void P_Select_Initialize(void)
 {
@@ -28,11 +26,18 @@ void P_Select_Initialize(void)
 	Select_1P = 0;
 	Select_2P = 0;
 	Select_Count = 0;
-	SelectPos.x = 450;
-    SelectPos.y = 100;
-	move_x = 0;
-	move_y = 0;
-	move = 0;
+	kaziki.pos.x = 450;
+	kaziki.pos.y = 100;
+	kaziki.flag = true;
+	kuzira.pos.x = 450 + SCREEN_WIDTH;
+	kuzira.pos.y = 100;
+	kuzira.flag = false;
+	iruka.pos.x = (450 + (SCREEN_WIDTH * 2));
+	iruka.pos.y = 100;
+	iruka.flag = false;
+	uma.pos.x = (450 + (SCREEN_WIDTH * 3));
+	uma.pos.y = 100;
+	uma.flag = false;
 	Select = false;
 	Left = false;
 	Right = false;
@@ -44,93 +49,33 @@ void P_Select_Finalize(void)
 
 void P_Select_Update(void)
 {
-	
-
-	if (Keyboard_IsTrigger(DIK_A) || GamePad_IsPress(2, BUTTON_A)) //カジキ
+	if (kaziki.flag)
 	{
-		if (Select_Count == 0)
+		if (Keyboard_IsTrigger(DIK_Y) || GamePad_IsTrigger(2, BUTTON_LB))
 		{
-			Select_1P = 1;
-			Select_Count++;
+			Left = true;
 		}
-		else if (Select_Count == 1)
+		if (Keyboard_IsTrigger(DIK_Y) || GamePad_IsPress(2, BUTTON_RB))
 		{
-			Select_2P = 1;
-			Select_Count++;
+
 		}
+
 	}
-	if (Keyboard_IsTrigger(DIK_B) || GamePad_IsPress(2, BUTTON_B)) //クジラ
+	if (Left)
 	{
-		if (Select_Count == 0)
+		if (kuzira.pos.x > 550)
 		{
-			Select_1P = 2;
-			Select_Count++;
+			kaziki.pos.x -= 25;
+			kuzira.pos.x -= 25;
+			iruka.pos.x -= 25;
+			uma.pos.x -= 25;
 		}
-		else if (Select_Count == 1)
+		else
 		{
-			Select_2P = 2;
-			Select_Count++;
+			Left = false;
 		}
-
 	}
-	if (Keyboard_IsTrigger(DIK_X) || GamePad_IsPress(2, BUTTON_X)) //イルカ
-	{
-		if (Select_Count == 0)
-		{
-			Select_1P = 3;
-			Select_Count++;
-		}
-		else if (Select_Count == 1)
-		{
-			Select_2P = 3;
-			Select_Count++;
-		}
 
-	}
-	if (Keyboard_IsTrigger(DIK_Y) || GamePad_IsPress(2, BUTTON_Y)) //馬
-	{
-		if (Select_Count == 0)
-		{
-			Select_1P = 4;
-			Select_Count++;
-		}
-		else if (Select_Count == 1)
-		{
-			Select_2P = 4;
-			Select_Count++;
-		}
-
-	}
-	if (Keyboard_IsTrigger(DIK_Y) || GamePad_IsPress(2, BUTTON_LB))
-	{		
-		//for (; move_x < SCREEN_WIDTH;)
-		//{
-			move_x += 1.5;
-		//}
-
-		//Left = true;
-	}
-	if (Keyboard_IsTrigger(DIK_Y) || GamePad_IsPress(2, BUTTON_RB))
-	{	
-		//for (; move_x > -SCREEN_WIDTH;)
-		//{
-			move_x -= 1.5;
-		//	//move += move_x;
-		//}
-
-		//Right = true;
-	}
-	//if (Right)
-	//{
-	//	Right = false;
-	//	//move_x = 0;
-	//}
-	//if (Left)
-	//{
-	//
-	//	Left = false;
-	//	//move_x = 0;
-	//}
 	if (Select_Count == 2)
 	{
 		Scene_Change(SCENE_INDEX_GAME);
@@ -139,35 +84,20 @@ void P_Select_Update(void)
 
 void P_Select_Draw(void)
 {
-	
+
 	Sprite_Draw(TEXTURE_INDEX_POOL, 0.0f, 0.0f);
 	Sprite_Draw(TEXTURE_INDEX_CHARASEN, 150.0f, -80.0f);
-	
+
 	Sprite_Draw(TEXTURE_INDEX_RIGHT, 1500.0f, 400.0f);
 	Sprite_Draw(TEXTURE_INDEX_LEFT, 150.0f, 400.0f);
-	
-	Sprite_Draw(TEXTURE_INDEX_KAZIKI_SELECT, SelectPos.x + move_x, SelectPos.y);
-	Sprite_Draw(TEXTURE_INDEX_KUJIRA_SELECT, SelectPos.x + SCREEN_WIDTH + move_x, SelectPos.y);
-	Sprite_Draw(TEXTURE_INDEX_IRUKA_SELECT, SelectPos.x + SCREEN_WIDTH * 2 + move_x, SelectPos.y);
-	Sprite_Draw(TEXTURE_INDEX_UMA_SELECT, SelectPos.x + SCREEN_WIDTH * 3 + move_x, SelectPos.y);
+
+	Sprite_Draw(TEXTURE_INDEX_KAZIKI_SELECT, kaziki.pos.x, kaziki.pos.y);
+	Sprite_Draw(TEXTURE_INDEX_KUJIRA_SELECT, kuzira.pos.x, kuzira.pos.y);
+	Sprite_Draw(TEXTURE_INDEX_IRUKA_SELECT, iruka.pos.x, iruka.pos.y);
+	Sprite_Draw(TEXTURE_INDEX_UMA_SELECT, uma.pos.x, uma.pos.y);
 
 }
 
-void Move()
-{
-	if (move_x < SCREEN_WIDTH)
-	{
-		move_x += 1.5;
-	}
-}
-
-void Move_()
-{
-	if (move_x > -SCREEN_WIDTH)
-	{
-		move_x -= 1.5;
-	}
-}
 
 int Get_Select_1P()
 {
--- HEW_Sample04/scene_playerselect.h
+++ HEW_Sample04/scene_playerselect.h
@@ -2,6 +2,30 @@
 
 #include <d3dx9.h>
 
+struct Select_Kaziki
+{
+	D3DXVECTOR2 pos;
+	bool flag;
+};
+
+struct Select_Kujira
+{
+	D3DXVECTOR2 pos;
+	bool flag;
+};
+
+struct Select_Iruka
+{
+	D3DXVECTOR2 pos;
+	bool flag;
+};
+
+struct Select_Uma
+{
+	D3DXVECTOR2 pos;
+	bool flag;
+};
+
 void P_Select_Initialize(void);
 void P_Select_Finalize(void);
 void P_Select_Update(void);
@@ -10,5 +34,3 @@ void P_Select_Draw(void);
 int Get_Select_1P();
 int Get_Select_2P();
 
-void Move();
-void Move_();
